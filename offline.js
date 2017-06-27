117cb9da377c5ca4d8e3cf34fc8e8a12fcf899de
ducthotran2010 <ducthotran2010@gmail.com>
2017-06-27 16:12:30 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,12 @@
+﻿{
+	"version": 1498550969,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/backgroundtile.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png"
+	]
+}
